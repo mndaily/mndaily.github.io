a942d933e3e3118e9ba78e46f13e13da8fc2724c
Ethan Nelson <enelson@mndaily.com>
2016-11-04 14:21:11 -0500

    Add files via upload

---
+++ polling/locations.js
@@ -0,0 +1,260 @@
+var places = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Holy Trinity Lutheran Church",
+        "address": "2730 E 31st St.",
+        "precinct": "2-1"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.2319974899292,
+          44.94730918410931
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Sullivan Community School",
+        "address": "3100 E 28th St.",
+        "precinct": "2-2"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.2257479429245,
+          44.95243640265157
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Van Cleve Park Recreation Center",
+        "address": "910 15th Ave. SE",
+        "precinct": "2-3"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.23099434375762,
+          44.985961785947566
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Weisman Art Museum",
+        "address": "333 River Parkway",
+        "precinct": "2-4"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.23715806007384,
+          44.97195778131947
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pratt Community School",
+        "address": "66 Malcom Ave. SE",
+        "precinct": "2-5"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.21406692266464,
+          44.96900703660774
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "St. Frances Cabrini Church",
+        "address": "1500 Franklin Ave. SE",
+        "precinct": "2-6"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.21755647659302,
+          44.96431589394198
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Augsburg College Christensen Center",
+        "address": "720 22nd Ave. S",
+        "precinct": "2-7"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.24063956737518,
+          44.967151120691014
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Brackett Park Recreation Park",
+        "address": "2728 39th Ave. S",
+        "precinct": "2-8"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.21702539920807,
+          44.95279515792491
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Oren Gateway Center",
+        "address": "610 22nd Ave. S",
+        "precinct": "2-9"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.24141204357147,
+          44.96746613723532
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Grace University Lutheran",
+        "address": "324 Harvard St. SE",
+        "precinct": "2-10"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.23062419891357,
+          44.972824948811926
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Longfellow Community School",
+        "address": "3017 E 31st St.",
+        "precinct": "2-11"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.22773277759552,
+          44.94626887803029
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "University Lutheran Church of Hope",
+        "address": "601 13th Ave. SE",
+        "precinct": "3-1"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.23576867580412,
+          44.98346138944131
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "First Congregational Church",
+        "address": "500 8th Ave. SE",
+        "precinct": "3-2"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.24275314807892,
+          44.98497909380399
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Marcy Open School",
+        "address": "415 4th Ave. SE",
+        "precinct": "3-3"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.25009167194366,
+          44.98659540476756
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Spring Manor Highrise",
+        "address": "828 Spring St. NE",
+        "precinct": "3-4"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.24986636638641,
+          44.9948621605537
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Ukrainian Event Center",
+        "address": "301 Main St. NE",
+        "precinct": "3-5"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.26109945774078,
+          44.99050319837008
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Brian Coyle Center",
+        "address": "420 15th Ave. S",
+        "precinct": "6-2"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.25055837631226,
+          44.97067313491736
+        ]
+      }
+    }
+  ]
+}
